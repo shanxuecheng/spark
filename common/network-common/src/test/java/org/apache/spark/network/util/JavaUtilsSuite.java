a67cbdbb5eb0b7a7a8a1f4ac6b2b88d0532aa36d
Yinghao Lin <yhcast0@apache.org>
2026-06-01 16:11:05 +0800

    AL-10373 Fix data corruption on block transfer via FetchBlockSegment (#883)

--- common/network-common/src/test/java/org/apache/spark/network/util/JavaUtilsSuite.java
+++ common/network-common/src/test/java/org/apache/spark/network/util/JavaUtilsSuite.java
@@ -16,12 +16,16 @@
  */
 package org.apache.spark.network.util;
 
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.junit.Test;
 import org.junit.Ignore;
-
-import static org.junit.Assert.*;
 
 public class JavaUtilsSuite {
 
@@ -54,5 +58,85 @@ public class JavaUtilsSuite {
     assertThrows(IOException.class,
       () -> JavaUtils.createDirectory(testDirPath, "scenario4"));
     assertTrue(testDir.setWritable(true));
+  }
+
+  @Test
+  public void testSkipFullyZeroBytes() throws Exception {
+    byte[] data = new byte[] {1, 2, 3, 4, 5};
+    InputStream in = new ByteArrayInputStream(data);
+    JavaUtils.skipFully(in, 0);
+    assertEquals(1, in.read());
+  }
+
+  @Test
+  public void testSkipFullyWithinAvailable() throws Exception {
+    byte[] data = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    InputStream in = new ByteArrayInputStream(data);
+    JavaUtils.skipFully(in, 3);
+    assertEquals(4, in.read());
+    assertEquals(5, in.read());
+    assertEquals(6, in.read());
+  }
+
+  @Test
+  public void testSkipFullyLargerThanSkipBuffer() throws Exception {
+    byte[] data = new byte[20000];
+    data[0] = 1;
+    data[9999] = 99;
+    data[10000] = 100;
+    data[19999] = 127;
+
+    InputStream in = new ByteArrayInputStream(data);
+    JavaUtils.skipFully(in, 10000);
+    assertEquals(100, in.read());
+  }
+
+  @Test
+  public void testSkipFullyExactSize() throws Exception {
+    byte[] data = new byte[] {1, 2, 3, 4, 5};
+    InputStream in = new ByteArrayInputStream(data);
+    JavaUtils.skipFully(in, 5);
+    assertEquals(-1, in.read());
+  }
+
+  @Test(expected = EOFException.class)
+  public void testSkipFullyBeyondEOF() throws Exception {
+    byte[] data = new byte[] {1, 2, 3};
+    InputStream in = new ByteArrayInputStream(data);
+    JavaUtils.skipFully(in, 10);
+  }
+
+  @Test
+  public void testSkipFullyThenReadRemaining() throws Exception {
+    byte[] data = new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    InputStream in = new ByteArrayInputStream(data);
+    JavaUtils.skipFully(in, 7);
+    byte[] remaining = new byte[3];
+    int bytesRead = 0;
+    while (bytesRead < remaining.length) {
+      int n = in.read(remaining, bytesRead, remaining.length - bytesRead);
+      if (n == -1) break;
+      bytesRead += n;
+    }
+    assertArrayEquals(new byte[] {7, 8, 9}, remaining);
+  }
+
+  @Test
+  public void testSkipFullySkipZeroIgnoresFirstReadError() throws Exception {
+    InputStream in = new InputStream() {
+      private int remaining = 10;
+      @Override
+      public int read() {
+        if (remaining <= 0) return -1;
+        remaining--;
+        return remaining;
+      }
+      @Override
+      public long skip(long n) {
+        return 0;
+      }
+    };
+    JavaUtils.skipFully(in, 5);
+    assertEquals(4, in.read());
   }
 }
